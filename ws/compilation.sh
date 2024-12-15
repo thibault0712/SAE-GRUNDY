6590126ad1d524dfe3e1db99a9d642ba22c33291
Thibault Falezan <76521322+thibault0712@users.noreply.github.com>
2024-12-15 15:47:22 +0100

    Update compilation.sh

--- ws/compilation.sh
+++ ws/compilation.sh
@@ -7,6 +7,8 @@ CLASS_DIR="../class"
 
 param1=$1 # all -> Compilation de tous les fichiers | workflow -> Compilation pour le workflow GitHub | none -> on compile les fichiers sauf SimpleInput et Start
 param2=$2 # debug -> Afficher les messages de compilation | none -> Ne pas afficher les messages de compilation
+output="null"
+
 
 echo -e "\033[1;34m=== Début de la compilation des fichiers Java dans '$SOURCE_DIR' ===\033[0m"
 
@@ -15,6 +17,16 @@ echo -e "\033[1;34m=== Début de la compilation des fichiers Java dans '$SOURCE_
 if [ "$param1" == "workflow" ]; then # Compilation pour le workflow GitHub
   javac ../src/*.java
   javadoc -encoding UTF8 -private -d ../javaDoc ../src/*.java
+  # Vérifier si la compilation a réussi
+  if [ $? -eq 0 ]; then
+      echo -e "\033[1;32m✅️ Compilation réussie"
+  elif [ $? -eq 1 ]; then
+      echo -e "\033[1;31m❌ Erreur de compilation"
+      exit 1
+  else
+      echo -e "\033[1;33m⚠️ Erreur inconnue lors de la compilation"
+      exit 2
+  fi
 elif [ "$param1" == "all" ]; then # Compilation de tous les fichiers
   for fichier in "$SOURCE_DIR"/*; do
     # Vérifier que l'élément est un fichier régulier
